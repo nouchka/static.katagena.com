d39d8340c2f8de88bbada9306327a70e9a15af80
nouchka <dev@zen-spirit.net>
2023-08-18 13:53:58 +0000

    local cache

--- kobe-fetch.sh
+++ kobe-fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+mkdir -p .cache/
+cd .cache/
 for i in {1..100}
 do
-	wget "http://www.city.kobe.lg.jp/information/public/online/onehundred-scenes/detail_img/img"$(echo $i|awk '{ printf "%03d\n", $1}')"_01.png"
+	wget -nc "http://www.city.kobe.lg.jp/information/public/online/onehundred-scenes/detail_img/img"$(echo $i|awk '{ printf "%03d\n", $1}')"_01.png"
 done
 
-mv img*.png web/kobe/
+cd ../
+cp .cache/img*.png web/kobe/
